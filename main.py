c5c439ff292c12fbea2fdd371eb18c9078c81793
moloch291 <Tunisforever19>
2020-07-04 01:25:42 +0200

    Printing method added.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import random
 from user_inputs import *
 
 
@@ -6,13 +5,4 @@ def main():
     print("Hi, this is a hangman game. Let's test your skills!")
 
 
-def choose_puzzle():
-    puzzle_pool = ["think about it", "catchphrase", "python", "pierce the veil", "apple tree", "consciousness",
-                   "Tunisia", "obsession", "fox", "tiredness", "Chicago", "chicken", "Nicholas Cage", "Aristotle",
-                   "pizza", "pasta", "Palermo", "hangman", "Ferrari", "water"]
-    puzzle = random.choice(puzzle_pool)
-    print(puzzle)
-    return puzzle
-
-
-get_user_inputs(choose_puzzle())
+get_guesses(hide_puzzle(choose_puzzle()))
--- user_inputs.py
+++ user_inputs.py
@@ -1,9 +1,30 @@
-def get_user_inputs(puzzle):
-    guess = input(" Take a guess! ")
+import random
+
+
+def choose_puzzle():
+    puzzle_pool = ["think about it", "catchphrase", "python", "pierce the veil", "apple tree", "consciousness",
+                   "Tunisia", "obsession", "fox", "tiredness", "Chicago", "chicken", "Nicholas Cage", "Aristotle",
+                   "pizza", "pasta", "Palermo", "hangman", "Ferrari", "water"]
+    puzzle = random.choice(puzzle_pool)
+    return puzzle
+
+
+def hide_puzzle(puzzle):
     for c in range(0, len(puzzle)):
-        if puzzle[c] == guess:
-            print(puzzle[c], end="")
-        elif puzzle[c] == " ":
+        if puzzle[c] == " ":
             print(" ", end="")
         else:
             print("_", end="")
+    return puzzle
+
+
+def get_guesses(puzzle):
+    for i in range(0, 10):
+        guess = input(" Take a guess! ")
+        for w in range(0, len(puzzle)):
+            if puzzle[w] == guess:
+                print(puzzle[w], end="")
+            elif puzzle[w] == " ":
+                print(" ", end="")
+            else:
+                print("_", end="")
