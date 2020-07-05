0a90fbf567eb5378ae137d4ee0bfa1f0c5da594e
moloch291 <Tunisforever19>
2020-07-06 00:00:31 +0200

    Loop created to register guesses and create the list of win condition letters.

--- user_inputs.py
+++ user_inputs.py
@@ -9,19 +9,13 @@ def choose_puzzle():
     return puzzle
 
 
-def get_guesses(puzzle):
-    win_cons = []
-    mistakes = []
-    for i in range(0, 16):
-        guess = input(" Take a guess! ")
-        for w in range(0, len(puzzle[0:])):
-            if puzzle[w] == guess or puzzle[w] in win_cons:
-                print(puzzle[w], end="")
-            elif puzzle[w] == " ":
-                print(" ", end="")
-            else:
-                print("_", end="")
-        if guess in puzzle:
-            win_cons.append(guess)
+def get_guess(puzzle, win_cons):
+    guess = input(" Take a guess! ")
+    for w in range(0, len(puzzle[0:])):
+        if puzzle[w] == guess or puzzle[w] in win_cons:
+            print(puzzle[w], end="")
+        elif puzzle[w] == " ":
+            print(" ", end="")
         else:
-            mistakes.append(guess)
+            print("_", end="")
+    return guess
